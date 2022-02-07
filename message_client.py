1f6eb4240327c6220445b240c2c5428fc7b1d65d
dale <sonicroblox@gmail.com>
2022-02-07 22:02:06 +0800

    zzzz

--- message_client.py
+++ message_client.py
@@ -2,6 +2,15 @@ import socket
 import sys
 import json
 
-msg_register = {"command":"register","username":"user01"}
-msg_deregister = {"command":"deregister","username":"user01"}
-msg_send = {"command":"msg","username":"user01","message":"This is my message."}
+username = input("Enter username: ")
+
+msg_register = {"command":"register","username":username}
+msg_deregister = {"command":"deregister","username":username}
+msg_send = {"command":"msg","username":username,"message":"This is my message."}
+
+server_addr = input("Enter server address: ")
+server_port = input("Port:")
+server_port = int(server_port)
+
+#create socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
