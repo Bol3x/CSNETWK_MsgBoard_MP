bc7fb702e0c99f8100837158ea904571539c2d2f
dale <sonicroblox@gmail.com>
2022-02-08 12:55:49 +0800

    added sockets

--- message_client.py
+++ message_client.py
@@ -3,14 +3,57 @@ import sys
 import json
 
 username = input("Enter username: ")
+msg_message = "This is my message."
 
 msg_register = {"command":"register","username":username}
 msg_deregister = {"command":"deregister","username":username}
-msg_send = {"command":"msg","username":username,"message":"This is my message."}
+msg_send = {"command":"msg","username":username,"message":msg_message} #might move this
 
 server_addr = input("Enter server address: ")
 server_port = input("Port:")
 server_port = int(server_port)
 
+buffer_size = 1024
+
 #create socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+try:
+    #register the user
+    print("Registering as " + username)
+    sock.sendto(json.dumps(msg_register), (server_addr, server_port))
+
+    recv, address = sock.recvfrom(1024)
+    #convert the received string into a json dict
+    recv_json = json.loads(recv)
+    recv_code = int(recv_json["ret_code"])
+
+except:
+    print("Error received, closing connection")
+    sock.close()
+
+if recv_code != 401:
+    print("Server returned error code " + recv_code)
+    print("Closing socket")
+    sock.close()
+
+print("Registration successful")
+
+#TODO how the fuck do we exit this
+while True:
+    msg_message = input("Enter message to send: ")
+    sock.sendto(json.dumps(msg_send), (server_addr, server_port))
+
+    recv, address = sock.recvfrom(1024)
+    #convert the received string into a json dict
+    recv_json = json.loads(recv)
+    recv_code = int(recv_json["ret_code"])
+
+    if recv_code != 401:
+        print("Server returned error code "  + recv_code)
+        break 
+
+
+#send a dereg message and then close socket
+sock.sendto(json.dumps(msg_deregister), (server_addr, server_port))
+sock.close()
